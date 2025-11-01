9fcbceef25f5827273a0afa4e7f79457270f8991
hnkphuong <phuonghnkk25406@st.uel.edu.vn>
2025-11-01 16:27:53 +0700

    xuly.py

--- xuly.py
+++ xuly.py
@@ -5,19 +5,19 @@ def tinhgpa(dsdiem,dstinchi):
         tongdiem+=dsdiem[i]*dstinchi[i]
         tongtinchi=sum(dstinchi)
         gpa=tongdiem/tongtinchi
-        print(gpa)
+    return gpa
 
 def xeploai(gpa):
-    if 9<=gpa<=10:
-        print("Xuất sắc")
-    if 8<=gpa<9:
-        print("Giỏi")
-    if 7<=gpa<8:
-        print("Khá")
-    if 5<=gpa<7:
-        print("Trung bình")
-    if 4<=gpa<5:
-        print("Yếu")
+    elif 9<=gpa<=10:
+        return "Xuất sắc"
+    elif 8<=gpa<9:
+        return "Giỏi"
+    elif 7<=gpa<8:
+        return "Khá"
+    elif 5<=gpa<7:
+        return "Trung bình"
+    elif 4<=gpa<5:
+        return "Yếu"
     else:
-        print("Kém")
+        return "Kém"
 
