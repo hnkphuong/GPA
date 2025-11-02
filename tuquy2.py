cc934de2b7f887461fb4fc09c3f60190413c8bce
hnkphuong <phuonghnkk25406@st.uel.edu.vn>
2025-11-02 21:52:36 +0700

    tuquy2.py

---
+++ tuquy2.py
@@ -0,0 +1,63 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from tuquy2 import Ui_MainWindow  # file .ui được convert thành .py
+
+class GiaodienChinh(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # gán nút tính và nút làm lại
+        self.ui.btnthem.clicked.connect(self.tinh_gpa)
+        self.ui.btnxoa.clicked.connect(self.lam_lai)
+
+        #lưu kết quả của tổng điểm và tổng tín chỉ khi người dùng nhấn nút thêm
+        self.tong_diem = 0
+        self.tong_tinchi = 0
+
+    def tinh_gpa(self):
+        try:
+            # gán biến "diem" và "tinchi" vào ô nhập liệu
+            diem = float(self.ui.lnediem.text()) 
+            tinchi = int(self.ui.lnetinchi.text()) 
+
+            # cộng dồn điểm và tín chỉ khi bấm nút thêm
+            self.tong_diem += diem * tinchi
+            self.tong_tinchi += tinchi
+
+            # tính GPA (làm tròn đến 2 chữ số thập phân)
+             gpa = round(self.tong_diem / self.tong_tinchi,2)
+
+            # hiển thị kết quả ra ô output
+            self.ui.lnekq.setText(str(gpa))
+
+            # xoá ô nhập sau khi thêm điểm và tín chỉ
+            self.ui.lnediem.clear()
+            self.ui.lnetinchi.clear()
+
+        except ValueError:
+            # hiển thị thông báo nếu nhập sai
+            self.ui.lnekq.setText("Lỗi! Nhập sai kiểu dữ liệu!")
+
+
+    def lam_lai(self):
+        # reset toàn bộ dữ liệu
+        self.tong_diem = 0
+        self.tong_tinchi = 0
+        self.ui.lnekq.setText("0")
+        self.ui.lnediem.clear()
+        self.ui.lnetinchi.clear()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = GiaodienChinh()
+    main.show()
+    sys.exit(app.exec())
+
+
+
+
+
+
